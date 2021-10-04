0599d260b1bda03a20e63971c832fadd5b1c01e4
HiroshiGTakamura <69849744+HiroshiGTakamura@users.noreply.github.com>
2021-10-04 10:53:44 -0300

    Add files via upload

---
+++ www/js/firebase.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+    apiKey: "AIzaSyA34Wh5_8U1XCuJXA5YuCpfswMnD4ekDyg",
+    authDomain: "cordova-420a2.firebaseapp.com",
+    projectId: "cordova-420a2",
+    storageBucket: "cordova-420a2.appspot.com",
+    messagingSenderId: "763778353944",
+    appId: "1:763778353944:web:ceb00610de663912296c6d",
+    measurementId: "G-6JH83580MY"
+  };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
